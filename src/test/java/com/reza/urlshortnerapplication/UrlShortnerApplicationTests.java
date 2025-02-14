8217d622116f92878ea72b1e718d502c085c05c7
Reza Hutagaol <silentsound05@gmail.com>
2025-02-15 01:56:08 +0700

    initial commit

---
+++ src/test/java/com/reza/urlshortnerapplication/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reza.urlshortnerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
